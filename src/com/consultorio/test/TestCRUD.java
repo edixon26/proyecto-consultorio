c98c080a96f424b54ba7ceb075b1f42ad9fdde75
Edixon Tunjo <tunjoedixon@gmail.com>
2025-07-09 00:33:47 -0500

    eliminar comentarios fastidiosos

--- src/com/consultorio/test/TestCRUD.java
+++ src/com/consultorio/test/TestCRUD.java
@@ -21,7 +21,7 @@ public class TestCRUD {
         Scanner sc = new Scanner(System.in);
         UsuarioDAO dao = new UsuarioDAO();
 
-        // Agregar un nuevo usuario
+        
         System.out.println("=== AGREGAR USUARIO ===");
         System.out.print("Ingrese nombre: ");
         String nombre = sc.nextLine();
@@ -32,20 +32,20 @@ public class TestCRUD {
         Usuario nuevo = new Usuario(0, nombre, correo);
         dao.insertarUsuario(nuevo);
 
-        // Consultar usuarios
+        
         System.out.println("\n=== LISTA DE USUARIOS ===");
         List<Usuario> usuarios = dao.consultarUsuarios();
         for (Usuario u : usuarios) {
             System.out.println(u.getId() + " | " + u.getNombre() + " | " + u.getCorreo());
         }
 
-        // Eliminar usuario por ID
+       
         System.out.println("\n=== ELIMINAR USUARIO ===");
         System.out.print("Ingrese el ID del usuario a eliminar: ");
         int idEliminar = sc.nextInt();
         dao.eliminarUsuario(idEliminar);
 
-        // Consultar usuarios después de eliminar
+        
         System.out.println("\n=== LISTA DE USUARIOS DESPUÉS DE ELIMINAR ===");
         usuarios = dao.consultarUsuarios();
         for (Usuario u : usuarios) {
